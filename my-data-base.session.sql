d98151f32dd02b10610609017299cbba78d170a9
Dgordgun <georgdvorskii@gmail.com>
2024-12-10 21:27:47 +0200

    UNIQUE

--- my-data-base.session.sql
+++ my-data-base.session.sql
@@ -1,8 +1,11 @@
--- CREATE TABLE users(
---     us_name varchar(100) NOT NULL CHECK(us_name != ''),
---     us_surname varchar(100) NOT NULL,
---     height numeric(3,2) CONSTRAINt my_name_for_constraint CHECK(height < 5.0)
--- );
+CREATE TABLE users(
+    us_name varchar(100) NOT NULL CHECK(us_name != ''),
+    us_surname varchar(100) NOT NULL,
+    height numeric(3,2) CONSTRAINT my_name_for_constraint CHECK(height BETWEEN 0 AND 5.0),
+    email varchar(300) UNIQUE NOT NULL
+);
 
-INSERT INTO users (us_name, us_surname, height) VALUES
-('', 'userovch', 4.99);
+INSERT INTO users (us_name, us_surname, height, email) VALUES
+('bkjl', 'userovch', 4.99, 'glikj');
+
+-- DROP TABLE users
