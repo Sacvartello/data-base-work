65512d7b40553516407e9424d02e3273742449c4
Dgordgun <georgdvorskii@gmail.com>
2024-12-10 20:31:31 +0200

    Create first table

---
+++ my-data-base.session.sql
@@ -0,0 +1,10 @@
+CREATE TABLE my_first_table(
+    us_name varchar(256),
+    us_surname varchar(256),
+    birth date,
+    pass varchar(300),
+    email varchar(300),
+    sub bool
+);
+INSERT INTO my_first_table VALUES
+('Mukola', 'Dvorskii','2014-03-11','qwerty','mikola123@gmail.com', TRUE);
