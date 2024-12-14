070c3382b7e918aa0329e2b737d67337bcaf247d
Dgordgun <georgdvorskii@gmail.com>
2024-12-14 20:34:40 +0200

    serial

--- my-data-base.session.sql
+++ my-data-base.session.sql
@@ -10,12 +10,26 @@
 
 -- DROP TABLE users
 
-CREATE TABLE messages(
-    author varchar(500) NOT NULL CHECK(author != ''),
-    message varchar(5000) NOT NULL,
-    is_seen bool DEFAULT FALSE,
-    created timestamp DEFAULT current_date
+-- CREATE TABLE messages(
+--     id serial NOT NULL UNIQUE,
+--     author varchar(500) NOT NULL CHECK(author != ''),
+--     message varchar(5000) NOT NULL,
+--     is_seen bool DEFAULT FALSE,
+--     created timestamp DEFAULT current_date
+-- );
+
+-- INSERT INTO messages (author, message) VALUES
+-- ('Ya','Hi!'),('Ti','Sho Hi?')
+
+-- DROP TABLE messages
+
+CREATE TABLE cars(
+    id serial NOT NULL UNIQUE,
+    brand varchar(256) NOT NULL CHECK(brand != ''),
+    made_year date,
+    volume int CHECK(volume >0 AND volume< 1500),
+    color varchar(100)
 );
 
-INSERT INTO messages (author, message) VALUES
-('Ya','Hi!'),('Ti','Sho Hi?')
+
+-- DROP TABLE messages
