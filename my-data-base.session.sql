72c93a1dc468c5fe032020a42b6500e1bc3da8b0
Dgordgun <georgdvorskii@gmail.com>
2024-12-19 21:12:48 +0200

    Homework 29(books and users) REFERENCES

--- my-data-base.session.sql
+++ my-data-base.session.sql
@@ -1,12 +1,13 @@
 -- CREATE TABLE users(
+--     id serial PRIMARY KEY,
 --     us_name varchar(100) NOT NULL CHECK(us_name != ''),
 --     us_surname varchar(100) NOT NULL,
---     height numeric(3,2) CONSTRAINT my_name_for_constraint CHECK(height BETWEEN 0 AND 5.0),
+--     phone int CONSTRAINT my_name_for_constraint,
 --     email varchar(300) UNIQUE NOT NULL
 -- );
 
 -- INSERT INTO users (us_name, us_surname, height, email) VALUES
--- ('bkjl', 'userovch', 4.99, 'glikj');
+-- ('us', 'userovch', 4.99, 'glikj');
 
 -- DROP TABLE users
 
@@ -23,13 +24,13 @@
 
 -- DROP TABLE messages
 
-CREATE TABLE cars(
-    id serial NOT NULL UNIQUE,
-    brand varchar(256) NOT NULL CHECK(brand != ''),
-    made_year date,
-    volume int CHECK(volume >0 AND volume< 1500),
-    color varchar(100)
-);
+-- CREATE TABLE cars(
+--     id serial NOT NULL UNIQUE,
+--     brand varchar(256) NOT NULL CHECK(brand != ''),
+--     made_year date,
+--     volume int CHECK(volume >0 AND volume< 1500),
+--     color varchar(100)
+-- );
 
 
 -- DROP TABLE messages
